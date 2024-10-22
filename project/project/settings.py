5ee24158232f7f24fa1668cbd40f1ebae5df6ec6
AksharaPalanichamy <akshupkram@gmail.com>
2024-10-23 02:53:30 +0530

    settings.py change

--- project/project/settings.py
+++ project/project/settings.py
@@ -143,3 +143,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/admin/' 
+SESSION_COOKIE_AGE = 300 
